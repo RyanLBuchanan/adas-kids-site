33ea1098b9178b23462f4e473fe5716f845bcf53
Ryan Buchanan <buchanan.ryan22@gmail.com>
2025-05-06 11:57:13 -0500

    feat: Updated the navbar js for mobile

--- js/script.js
+++ js/script.js
@@ -76,3 +76,14 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("mobile-menu-button");
+  const menu = document.getElementById("mobile-menu");
+
+  if (btn && menu) {
+    btn.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
+    });
+  }
+});
