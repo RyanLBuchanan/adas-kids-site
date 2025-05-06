b8d90820ff4f5476ddae01e2889518348a90d65b
Ryan Buchanan <buchanan.ryan22@gmail.com>
2025-05-06 12:01:00 -0500

    feat: Fellback navbar/menu js to keep mobile functionality

--- js/script.js
+++ js/script.js
@@ -77,13 +77,13 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("mobile-menu-button");
-  const menu = document.getElementById("mobile-menu");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const btn = document.getElementById("mobile-menu-button");
+//   const menu = document.getElementById("mobile-menu");
 
-  if (btn && menu) {
-    btn.addEventListener("click", () => {
-      menu.classList.toggle("hidden");
-    });
-  }
-});
+//   if (btn && menu) {
+//     btn.addEventListener("click", () => {
+//       menu.classList.toggle("hidden");
+//     });
+//   }
+// });
