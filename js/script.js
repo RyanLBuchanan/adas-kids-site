3311322bd391682c1e7b6f88a8a866fa4b6ec02d
Ryan Buchanan <buchanan.ryan22@gmail.com>
2025-05-08 10:15:32 -0500

    feat: Removed extraneous mobile menu code

--- js/script.js
+++ js/script.js
@@ -76,25 +76,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const btn = document.getElementById("mobile-menu-button");
-//   const menu = document.getElementById("mobile-menu");
-
-//   if (btn && menu) {
-//     btn.addEventListener("click", () => {
-//       menu.classList.toggle("hidden");
-//     });
-//   }
-// });
-
-// Mobile menu toggle
-window.addEventListener("load", () => {
-  const btn = document.getElementById("mobile-menu-button");
-  const menu = document.getElementById("mobile-menu");
-  if (btn && menu) {
-    btn.addEventListener("click", () => {
-      menu.classList.toggle("hidden");
-    });
-  }
-});
